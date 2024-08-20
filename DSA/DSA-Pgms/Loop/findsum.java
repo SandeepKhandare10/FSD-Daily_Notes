db4cabcbe71f8e8c0519083d1ef454766207fbbc
SandeepKhandare10 <sandipkhandare10@gmail.com>
2024-08-20 21:00:45 +0530

    20/8/24

---
+++ DSA/DSA-Pgms/Loop/findsum.java
@@ -0,0 +1,61 @@
+package Loop;
+
+public class findsum {
+    static int[] findtwosum(int[] arr, int key) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] + arr[j] == key) {
+                    return new int[]{arr[i], arr[j]};
+                }
+            }
+        }
+        return new int[]{-1, -1};
+    }
+    static int Unicnumber(int []arr){
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    arr[i] = -1;
+                    arr[j] = -1;
+
+                }
+            }
+        }
+        int ans=-1;
+        for (int i=0;i< arr.length;i++){
+            if (arr[i]>0){
+                ans=arr[i];
+            }
+        }
+        return ans;
+    }
+    static int[]findthreesum(int []arr,int key){
+        for (int i=0;i< arr.length-2;i++){
+            for(int j=i+1;j< arr.length-1;j++){
+                for (int k=j+1;k< arr.length;k++){
+                    if (arr[i]+arr[j]+arr[k]==key){
+                        return new int[]{arr[i],arr[j],arr[k]};
+                    }
+                }
+            }
+        }
+        return new int[]{-1, -1,-1};
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{14, 85, 78, 45, 9, 8, 6, 4};
+        int[] result = findthreesum(arr,18);
+        if (result[0] != -1) {
+            System.out.println(STR."Pair found: \{result[0]}, \{result[1]}, \{result[2]}");
+        } else {
+            System.out.println("No pair found");
+        }
+        int[]ans=findtwosum(arr,10);
+        if (ans[0]!=-1){
+            System.out.println(ans[0]+" "+ans[1]);
+        }
+        System.out.println(Unicnumber(arr));
+//        System.out.println(arr.Math.Min);
+
+    }
+}
---
+++ DSA/DSA-Pgms/Searchingandsorting/Bubblesort.java
@@ -0,0 +1,22 @@
+package Searchingandsorting;
+
+public class Bubblesort {
+    public static void main(String[] args) {
+        int temp = 0;
+        int[] arr = {5, 8, 6, 9, 8, 4, 3, 2, 1, 12, 45, 69, 85, 45};
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
