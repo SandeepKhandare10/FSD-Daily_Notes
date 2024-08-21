82edd2e75d6de3edb1fbc6320b2ed19d0ab1c55e
SandeepKhandare10 <sandipkhandare10@gmail.com>
2024-08-21 14:20:57 +0530

    21/8

---
+++ DSA/DSA-Pgms/Searchingandsorting/lexicografic.java
@@ -0,0 +1,22 @@
+package Searchingandsorting;
+
+public class lexicografic {
+    public static void main(String[] args) {
+        String [] frutrs={"pappaya","apple","banana","mango","kiwi"};
+        for (int i=0;i< args.length-1;i++){
+            int min=i;
+            for (int j=i+1;j< frutrs.length;j++){
+                if (frutrs[j].compareTo(frutrs[j+1])<0){
+                    min=j;
+                }
+            }
+            String temp=frutrs[i];
+            frutrs[i]=frutrs[min];
+            frutrs[min]=temp;
+        }
+    for (String val:frutrs){
+        System.out.println(val);
+    }
+    }
+
+}
